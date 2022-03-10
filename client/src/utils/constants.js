a08c756869175310973bed972e72700b73a7e8a5
Vkar90 <kardamitsis.e@gmail.com>
2022-03-10 17:45:50 +0200

    finish all blockchain transactions setup

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json'
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x7A3B9c883c509AA228a82f5361EcDF48fF87e4d0';
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,21 +1,11 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle')
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
-};
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/Nr75rNsNCK06oBikRmTdB0m9sXA_mMZp',
+      accounts: ['33f3d8e20e6f7ba5654cca4968d532b23188fee3663bcb3e1c929a309df8e083']
+    }
+  }
+}
