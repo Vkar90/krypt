7ecdbabd2a85c946ef5d12161e5d16c8634420bd
Vkar90 <kardamitsis.e@gmail.com>
2022-03-11 10:22:12 +0200

    create hook for gif search logic

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -5,6 +5,7 @@ import { BsInfoCircle } from 'react-icons/bs'
 
 import { TransactionContext } from "../context/TransactionContext"
 import { Loader } from "."
+import { shortenAddress } from "../utils/shortenAddress"
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
@@ -69,7 +70,7 @@ const Welcome = () => {
                 </div>
                 <div>
                   <p className="text-white font-light text-sm">
-                  Address
+                  {shortenAddress(currentAccount)}
                   </p>
                   <p className="text-white font-semibold text-lg mt-1">
                     Ethereum
@@ -80,7 +81,7 @@ const Welcome = () => {
           <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
             <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
             <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
-            <Input placeholder="Keyword (GIF) To" name="keyword" type="text" handleChange={handleChange} />
+            <Input placeholder="Keyword (GIF)" name="keyword" type="text" handleChange={handleChange} />
             <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
             <div className="h-[1px] w-full bg-gray-400 my-2" />
             {false ? (
